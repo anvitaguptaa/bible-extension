788d01c75ac2ce5280de07381020c80880e5a5ae
Anvita Gupta <anvitagupta@Anvitas-MacBook-Pro.local>
2022-11-14 00:33:52 -0500

    Added character images

--- popup/popup.js
+++ popup/popup.js
@@ -6,19 +6,49 @@ var quotes = {
 
     "Katherine" : ["I support women's wrongs", 
                    "It's like, even if I was cheating why is it any of your business??",
-                   "#induce addiction onto your exes >:)"],
+                   "induce addiction onto your exes >:)"],
                    
     "Anvi" : ["You don't have to ask &#x1FAF3;",
-              "Once I learn how to punch myself in the face, it's over for men.",
-              "Jail."]
+              "Once I learn how to punch myself in the face, it's over for men.", "Jail."]
 }; 
 
 document.getElementById("generate-button").addEventListener("click", myFunction);
 
+function getImageCharacter(name) {
+    switch(name) {
+        case "Anvi":
+            image = "/images/anvi.png";
+            break;
+        case "Alicia":
+            image = "/images/alicia.png";
+            break;
+        case "Katherine":
+            image = "/images/katherine.png";
+            break;
+    }
+    return image;
+}
+
 function myFunction() {
     var name = names[Math.floor(Math.random() * names.length)];
     var quote = quotes[name][Math.floor(Math.random() * quotes[name].length)];
+    var src_image = getImageCharacter(name);
 
     document.getElementById('display-label').innerHTML = quote.toUpperCase();
+
+    let image = document.getElementById("character-img");
+    image.src = src_image;
+
+    switch(name) {
+        case "Anvi":
+            image.style.width = "50px";
+            break;
+        case "Alicia":
+            image.style.width = "35px";
+            break;
+        case "Katherine":
+            image.style.width = "45px";
+            break;
+    }
 }
 
