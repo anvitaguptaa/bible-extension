d0742a953886a793f14ddf2b4bf96ee03f0afc19
Anvita Gupta <anvitagupta@Anvitas-MBP.cogeco.local>
2022-11-13 21:15:45 -0500

    Added js functionality to display messages

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,21 @@
-function addTextbox(event) {
-    var input = document.createElement("input");
-    input.setAttribute("type", "text");
-    document.getElementById("textboxContainer").appendChild(input);
+var names = ["Katherine", "Alicia", "Anvi"];
+
+var quotes = {
+    "Alicia" : ["Live love laugh objectify", 
+                "I think I've found a green flag that cancels out his racism"],
+    "Katherine" : ["I support women's wrongs", 
+                   "Even if I was cheating why is it any of your business",
+                   "#induce addiction onto your exes"],
+    "Anvi" : ["You don't have to ask &#x1FAF3;",
+              "Once I learn how to punch myself in the face it's over for men"]
+}; 
+
+document.getElementById("generate-button").addEventListener("click", myFunction);
+
+function myFunction() {
+    var name = names[Math.floor(Math.random() * names.length)];
+    var quote = quotes[name][Math.floor(Math.random() * quotes[name].length)];
+
+    document.getElementById('display-label').innerHTML = quote;
 }
+
